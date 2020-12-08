7d9f3ade9a6eb43e4e6b0d6ff5c64dab4299ff2c
mgschiavon <mgschiavon@users.noreply.github.com>
2020-12-08 13:45:30 -0800

    First parameter's file
    Includes definition of synthesis rate functions for upstream genes.

--- InputFiles/ARGS_DN_A_Par_Ex01.jl
+++ InputFiles/ARGS_DN_A_Par_Ex01.jl
@@ -1,15 +1,39 @@
 # Kinetic parameters
 p = Dict([
-    :g   => 0.01,      # Dilution rate (e.g. [0.01,0.24] 1/min)
-    :mY  => 0.125,     # Y synthesis rate dependent of W (nM/min)
-    :gY  => 1,         # Y degradation rate (e.g. [0.0301,0.0408] 1/min, with degODC)
-    :mU  => 0.125,     # U synthesis rate dependent of Y (nM/min)
-    :gU  => 0.0001,    # U degradation rate (e.g. [0.0301,0.0408] 1/min, with degODC)
-    :mW  => 0.1,       # W constitutive synthesis rate (e.g. [0.005,0.02] nM/min)
-    :gW  => 0.0001,    # W degradation rate (e.g. [0.0301,0.0408] 1/min, with degODC)
-    :e0  => 0.0001,    # U:W dissociation (unbinding) rate (e.g. [0.05,140] 1/min)
-    :eP  => 0.0375,    # U:W association (binding) rate (e.g. [0.0012,2000] 1/nM 1/min)
-    :eM  => 0.5,       # U:W (C) mutual anhilation/co-degradation (e.g. [0.03,2] 1/nM 1/min)
-    :mYs => NaN,       # LOCAL: Ys constitutive synthesis rate (e.g. [0.005,0.02] nM/min)
-    :gYs => NaN,       # LOCAL: Ys constitutive degradation rate (e.g. [0.0301,0.0408] 1/min, with degODC)
+    :g  => 0.01,      # Dilution rate (e.g. [0.01,0.24] 1/min)
+	:xE => 0.0607,    # GEM iSynTF concentration (nM)
+	:hE => 1.0,       # E2 concentration (nM)
+	:mE => 0.0159,    # Maximum synthesis rate regulated by GEM
+	:kXE => 139.0,    # GEM:E2 dissociation constant
+	:bE => 1.02e-4,   # Fraction of the inactive GEM in the nucleus
+	:aE => 0.0048,    # Basal expression (in the absence of GEM)
+	:nE => 1.4,       # Hill coefficient for GEM regulation
+	:kE => 0.0073,    # GEM:promoter dissociation constant
+	:xP => 0.3042,    # Z3PM iSynTF concentration (nM)
+	:hP => 1.0,       # E2 concentration (nM)
+	:mP => 3.1445,    # Maximum synthesis rate regulated by Z3PM
+	:kXP => 32.5,     # Z3PM:Pg dissociation constant
+	:bP => 0.0317,    # Fraction of the inactive Z3PM in the nucleus
+	:aP => 0.0153,    # Basal expression (in the absence of Z3PM)
+	:nP => 2.64,      # Hill coefficient for Z3PM regulation
+	:kP => 0.0683,    # Z3PM:promoter dissociation constant
+	:mA => NaN,	      # A synthesis rate (nM/min)
+	:mD => NaN,	      # D synthesis rate (nM/min)
+	:mN => NaN,	      # N synthesis rate (nM/min)
+	:mY => 0.1,       # Y maximum synthesis rate (nM/min)
+	:a  => 0.01,      # Basal activity
+	:n  => 2.5,       # Hill function
+	:k  => 1.0,       # Dissociation constant
+	:eP => 0.0375,    # A:D association (binding) rate (e.g. [0.0012,2000] 1/nM 1/min)
+	:eM => 0.0001,    # A:D dissociation (unbinding) rate (e.g. [0.05,140] 1/min)
+	:bP => 0.375,     # A:N association (binding) rate (e.g. [0.0012,2000] 1/nM 1/min)
+	:bM => 0.0001,    # A:N dissociation (unbinding) rate (e.g. [0.05,140] 1/min)
 ]);
+
+# Calculate/update synthesis rate according to gene regulation (e.g. iSynTF_mu(x,h,kX,b,m,a,n,k)):
+function pSynth(p,iSynTF_mu)
+	p[:mA] = iSynTF_mu(p[:xE],p[:hE],p[:kXE],p[:bE],p[:mE],p[:aE],p[:nE],p[:kE]);
+	p[:mD] = 0.1;
+	p[:mN] = iSynTF_mu(p[:xP],p[:hP],p[:kXP],p[:bP],p[:mP],p[:aP],p[:nP],p[:kP]);
+	return;
+end
