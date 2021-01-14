86896fbbec54df044b9bed4fcf0356e18f81c805
mgschiavon <mgschiavon@users.noreply.github.com>
2021-01-14 11:31:41 -0800

    Consolidating Dominant Negative systems
    ...in just one ODE file.

--- Library/Md_DNH.jl
+++ Library/Md_DNH.jl
@@ -1,5 +1,6 @@
 # Heterodimer competition model
 #   with DN (N) binding AD (A) part
+#   and with DN (N) binding DBD (D) part
 
 # Julia v.1.1.1
 
@@ -8,8 +9,8 @@ module mm
 	using DifferentialEquations
 	using ParameterizedFunctions
 
-	# ODE system
-	myODE = @ode_def begin
+	# ODE system -- N:A vs A:D
+	myODE_A = @ode_def begin
 		dY  = (mY * ((aY * (kY^nY)) + (AD^nY))/((kY^nY) + (AD^nY) + (D^nY))) - (g * Y)
 		dA  = mA - (g * A) + (eM * AD) - (eP * A * D) + (bM * AN) - (bP * A * N)
 		dD  = mD - (g * D) + (eM * AD) - (eP * A * D)
@@ -17,4 +18,14 @@ module mm
 		dAD =   - (g * AD) - (eM * AD) + (eP * A * D)
 		dAN =   - (g * AN)                            - (bM * AN) + (bP * A * N)
 	end g mA mD mN mY aY nY kY eP eM bP bM;
-end 
+
+	# ODE system -- N:D vs A:D
+	myODE_D = @ode_def begin
+		dY  = (mY * ((aY * (kY^nY)) + (AD^nY))/((kY^nY) + (AD^nY) + (D^nY) + (DN^nY))) - (g * Y)
+		dA  = mA - (g * A) + (eM * AD) - (eP * A * D)
+		dD  = mD - (g * D) + (eM * AD) - (eP * A * D) + (bM * DN) - (bP * D * N)
+		dN  = mN - (g * N)                            + (bM * DN) - (bP * D * N)
+		dAD =   - (g * AD) - (eM * AD) + (eP * A * D)
+		dDN =   - (g * DN)                            - (bM * DN) + (bP * D * N)
+	end g mA mD mN mY aY nY kY eP eM bP bM;
+end
