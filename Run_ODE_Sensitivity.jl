eb56d5aeea4586dc8f5a5999e4975e6ee0d31661
mgschiavon <>
2021-02-24 15:48:54 -0800

    Non-competition (simpler) model - HDM
    As a control

--- Run_ODE_Sensitivity.jl
+++ Run_ODE_Sensitivity.jl
@@ -1,6 +1,6 @@
 ## Running in julia terminal
 	cd("C:\\Users\\mgsch\\Dropbox (MGS-UCSF)\\MODEL - Heterodimer feedback\\HDM\\")
-	iARG = (mm = "HDM",	# Label for motif file
+	iARG = (mm = "HDMv2",	# Label for motif file
 			ex = "Ex01");	# Label for parameters file
 			#include("Run_ODE_Sensitivity.jl")
 
